7ce92106b3d8b4267a0d1ab82bb7026f7381a83c
coltsfan <coltsfan@BLUE-SURFACE>
2016-05-07 14:53:51 -0500

    Add project files.

---
+++ Nerdery_coinsorter/Controllers/CoinsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Nerdery_coinsorter.Models;
+
+namespace Nerdery_coinsorter.Controllers
+{
+    public class CoinsController : ApiController
+    {
+        Coin[] coins = new Coin[]
+        {
+            new Coin { CoinType = "Silver Dollar", CoinValue = 1.00m  },
+            new Coin { CoinType = "half dollar", CoinValue = 0.50m  },
+            new Coin { CoinType = "quarter", CoinValue = 0.25m  },
+            new Coin { CoinType = "dime", CoinValue = 0.10m  },
+            new Coin { CoinType = "nickel", CoinValue = 0.05m  },
+            new Coin { CoinType = "penny", CoinValue = 0.01m  },
+        };
+
+        public IEnumerable<Coin> GetAllCoins()
+        {
+            return coins;
+        }
+        
+        public IHttpActionResult GetCoins(decimal totalamount)
+        {
+            decimal amt_remaining = totalamount;
+            foreach (var coin in coins)
+            {
+                if (amt_remaining < coin.CoinValue) continue;
+                var coin_quotient = (int)(amt_remaining / coin.CoinValue);
+                //return Ok(new string('-', 28));
+                //return Ok("{0,10}{1,15}", coin.CoinType, coin_quotient);
+                Console.WriteLine(new string('-', 28));
+                Console.WriteLine("{0,10}{1,15}", coin.CoinType, coin_quotient);
+                amt_remaining -= coin_quotient * coin.CoinValue;
+                if (amt_remaining <= 0) break;
+            }
+            // return Ok(new string('-', 28));
+            Console.WriteLine(new string('-', 28));
+            return Ok(amt_remaining);
+        }
+    }
+}
---
+++ Nerdery_coinsorter/Models/Coin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nerdery_coinsorter.Models
+{
+    public class Coin
+    {
+        public string CoinType { get; set; }
+        public decimal CoinValue { get; set; }
+        //public int CoinCount { get; set; }
+
+
+        //public static int GetCoinCount(int coin_total, int[] coin_array)
+        //{
+        //    const int MAX = Int32.MaxValue;
+
+        //    int[] coin_count = new int[coin_total + 1];
+        //    coin_count[0] = 0;
+
+        //    for (int x = 1; x <= coin_total; x++)
+        //    {
+        //        int total = MAX;
+        //        for(int y = 0; y < coin_array.Length; y++)
+        //        {
+        //            if (x - coin_array[y] >= 0 && total > coin_count[x - coin_array[y]])
+        //                total = coin_count[x - coin_array[y]];
+        //        }
+        //        if (total < MAX)
+        //            coin_array[x] = total + 1;
+        //        else
+        //            coin_array[x] = MAX;
+        //    }
+        //    return coin_count[coin_total];
+        //}
+        //===============================Testing=================//
+        //private static void SampleRun(String TestCase, int coin_total,int[] coin_array, int Answer)
+        //{
+        //    Console.Write(TestCase + " starting: ");
+
+        //    if (GetCoinCount(coin_total, coin_array) == Answer)
+        //        Console.Write("Success.\n");
+        //    else
+        //        Console.Write("Error.\n");
+        //}
+        //private static void Case1()
+        //{ 
+        //    int[] coin_array = { 1, 3, 9, 10 };
+        //    int coin_total = 15;
+        //    int Answer = 3;
+
+        //    SampleRun("Case1", coin_total, coin_array, Answer);
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Case1();
+        //}
+
+    }
+}
