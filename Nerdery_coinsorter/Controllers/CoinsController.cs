819d45658e2932d52c1de71f735f77be61582106
jollin shah <jollinshah@gmail.com>
2016-05-09 15:12:05 -0500

    changes on 05_09_2016 @ 15:11

--- Nerdery_coinsorter/Controllers/CoinsController.cs
+++ Nerdery_coinsorter/Controllers/CoinsController.cs
@@ -12,36 +12,17 @@ namespace Nerdery_coinsorter.Controllers
     {
         Coin[] coins = new Coin[]
         {
-            new Coin { CoinType = "Silver Dollar", CoinValue = 1.00m  },
-            new Coin { CoinType = "half dollar", CoinValue = 0.50m  },
-            new Coin { CoinType = "quarter", CoinValue = 0.25m  },
-            new Coin { CoinType = "dime", CoinValue = 0.10m  },
-            new Coin { CoinType = "nickel", CoinValue = 0.05m  },
-            new Coin { CoinType = "penny", CoinValue = 0.01m  },
+            new Coin { CoinType = "Silver Dollar", CoinValue = 1.00M  },
+            new Coin { CoinType = "Half dollar", CoinValue = 0.50M  },
+            new Coin { CoinType = "Quarter", CoinValue = 0.25M  },
+            new Coin { CoinType = "Dime", CoinValue = 0.10M  },
+            new Coin { CoinType = "Nickel", CoinValue = 0.05M  },
+            new Coin { CoinType = "Penny", CoinValue = 0.01M },
         };
 
         public IEnumerable<Coin> GetAllCoins()
         {
             return coins;
-        }
-        
-        public IHttpActionResult GetCoins(decimal totalamount)
-        {
-            decimal amt_remaining = totalamount;
-            foreach (var coin in coins)
-            {
-                if (amt_remaining < coin.CoinValue) continue;
-                var coin_quotient = (int)(amt_remaining / coin.CoinValue);
-                //return Ok(new string('-', 28));
-                //return Ok("{0,10}{1,15}", coin.CoinType, coin_quotient);
-                Console.WriteLine(new string('-', 28));
-                Console.WriteLine("{0,10}{1,15}", coin.CoinType, coin_quotient);
-                amt_remaining -= coin_quotient * coin.CoinValue;
-                if (amt_remaining <= 0) break;
-            }
-            // return Ok(new string('-', 28));
-            Console.WriteLine(new string('-', 28));
-            return Ok(200);
         }
     }
 }
